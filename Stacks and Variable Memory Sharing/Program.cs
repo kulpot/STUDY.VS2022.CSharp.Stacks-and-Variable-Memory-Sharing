94c7bc8783d63baa707d4b0dc93f13c3f97116fd
Kulpot <kulpotwahu@gmail.com>
2023-09-09 23:28:38 +0800

    Add project files.

---
+++ Stacks and Variable Memory Sharing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=H1FXz_EaZds&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=44
+//
+
+class MainClass
+{
+    static void Main()
+    {
+        OneMethod();
+        TwoMethod();
+    }
+    static void OneMethod()
+    {
+        short s = 8;
+        Console.WriteLine(s);
+    }
+    static void TwoMethod()
+    {
+        char c = 'j';
+        Console.WriteLine(c);
+    }
+}
